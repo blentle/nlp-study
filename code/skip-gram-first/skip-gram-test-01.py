b47dda979fb3b5a38732627931e8e001d3278dbb
haun.ren <huan.ren@ipinyou.com>
2020-04-20 08:55:39 +0800

    update dataset directory

--- code/skip-gram-first/skip-gram-test-01.py
+++ code/skip-gram-first/skip-gram-test-01.py
@@ -1,5 +1,5 @@
 import csv
-with open('E:\\后场nlp学习\\数据集\\AutoMaster_TrainSet.csv', 'r', encoding='utf-8') as f:
+with open('E:\\nlp-dataset\\AutoMaster_TrainSet.csv', 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
         print(row)
