0a365c111bdfacd3d28095e70a1d3f1ae3bc3980
haun.ren <huan.ren@ipinyou.com>
2020-05-19 12:36:49 +0800

    优化字典构建代码

--- code/skip-gram-first/build_vocabs.py
+++ code/skip-gram-first/build_vocabs.py
@@ -1,15 +1,47 @@
-def build_vocabs(train_x_cut_list, train_y_cut_list, test_x_cut_list):
-    pass
+from collections import Counter
+
+
+## 构建正向、反向字典 word2id, id2word,并去掉低频词,频数低于 min_count的词会被去掉
+def build_vocabs(train_x_cut_list, train_y_cut_list, test_x_cut_list, min_count):
+    print("begin to build vocabs...")
+    words_all = []
+    words_all += train_x_cut_list
+    words_all += train_y_cut_list
+    words_all += test_x_cut_list
+    ## 统计词频数,封装成字典
+    words_dict = dict(Counter(words_all))
+    ## 去掉低频次
+    words_dict = {w: cnt for w, cnt in words_dict if cnt > min_count}
+    print("去掉低频次以后还剩下 " + str(len(words_dict)) + " 单词")
+    ## 按每个词的频数降序排列
+    p = sorted(words_dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+    ## 排列后的序号作为id，生成words2id的字典和id2words的字典
+    words2id = {}
+    id2words = {}
+    for index, dict_data in p:
+        ## 软件强迫症，id 从1 开始
+        words2id[index + 1] = dict_data[0]
+        id2words[dict_data[0]] = index + 1
+    ## 也返回词的频数字典
+    return words2id, id2words, p
 
 
 ## 读取切完词保存的路径，每个词按空格分隔，读取到的每个词放在list中返回:["word1","word2"]
-def read_data(path):
+def read_words_to_list(file_path):
     words = []
-    with open(path, 'r', encoding='utf-8') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         for line in f:
             ## 去掉空格
             line = line.strip()
             ## 去掉空字符串
             p = [w for w in line.split() if w.strip() != '']
             words += p
+    f.close()
     return words
+
+
+if __name__ == '__main__':
+    list = ["a", 'b', 'c', 'c', 'c', 'a']
+    dict = dict(Counter(list))
+    p = sorted(dict.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+
